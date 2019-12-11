91299b550c331f33782fe1f2d3f05d5cce10aea8
Chaurasia <765406@cognizant.com>
2019-12-11 10:35:57 +0530

    _

--- TypescriptTest/app/src/decorators.ts
+++ TypescriptTest/app/src/decorators.ts
@@ -1,9 +1,42 @@
+/**
+ * 
+ * @param constructor 
+ * @name decorators training
+ * @author Jatinder Kumar Chaurasia
+ */
+
+
+
+
 // Decorators 
+// function Logger(constructor: Function) {
+//     console.log('Logging..............');
+//     console.log(constructor);
+// }
 
+// Factory Decorators
+function Logger1(logString: string) {
+    return function (constructor: Function) {
+        console.log(logString);
+        console.log(constructor);
+    }
+}
 
-class Person{
+function WithTemplate(template: string, hookId: string) {
+    return function (constructor: Function) {
+        const element = document.getElementById(hookId);
+        if(element){
+            
+        }
+    }
+}
+
+// @Logger
+// @Logger1('Hello I am Logging................... !')
+@WithTemplate('', 'hookApp')
+class Person {
     name = 'Max';
-    constructor(){
+    constructor() {
         console.log('Creating new Person !!');
     }
 }
