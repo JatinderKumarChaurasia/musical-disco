5975c65385a272a3ef31fc1c2eb3e16ce9804377
Chaurasia <765406@cognizant.com>
2019-12-11 10:47:39 +0530

    _

--- TypescriptTest/app/src/decorators.ts
+++ TypescriptTest/app/src/decorators.ts
@@ -8,32 +8,49 @@
 
 
 
-// Decorators 
+// Factory Decorators
+
 // function Logger(constructor: Function) {
 //     console.log('Logging..............');
 //     console.log(constructor);
 // }
 
-// Factory Decorators
-function Logger1(logString: string) {
+function Logger(logString: string) {
+    console.log('Rendering with Logger Factory !!.......');
     return function (constructor: Function) {
+        console.log('Rendering with Logger !!.......');
         console.log(logString);
         console.log(constructor);
     }
 }
 
+// function WithTemplate(template: string, hookId: string) {
+//     return function (_: Function) {
+//         const element = document.getElementById(hookId);
+//         if(element){
+//             element.innerHTML = template;
+//         }
+//     }
+// }
+
+
 function WithTemplate(template: string, hookId: string) {
-    return function (constructor: Function) {
+    console.log('Rendering with Template Factory !!.......');
+    return function (constructor: any) {
+        console.log('Rendering with Template !!.......');
         const element = document.getElementById(hookId);
+        const p = new constructor();
         if(element){
-            
+            element.innerHTML = template;
+            element.querySelector('h1')!.textContent = p.name;
         }
     }
 }
 
+
 // @Logger
-// @Logger1('Hello I am Logging................... !')
-@WithTemplate('', 'hookApp')
+ @Logger('Hello I am Logging................... !')
+@WithTemplate('<h1> Hooking Template </h1>', 'hookApp')
 class Person {
     name = 'Max';
     constructor() {
