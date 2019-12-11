782e23d50be71eadaf4379d3730319e9dc3e110e
Chaurasia <765406@cognizant.com>
2019-12-11 10:24:35 +0530

    _

--- TypescriptTest/app/src/decorators.ts
+++ TypescriptTest/app/src/decorators.ts
@@ -1,3 +1,11 @@
+// Decorators 
+
+
 class Person{
-    
+    name = 'Max';
+    constructor(){
+        console.log('Creating new Person !!');
+    }
 }
+const person = new Person();
+console.log(person);
